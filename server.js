e0fffa7ebac16790a929a64002d10a8c16ec55e8
Harish Kunwar <kunwarharish11@gmail.com>
2024-02-28 21:59:01 +0545

    Add database connection

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const app=require("./app");
+const dbConnect=require("./config/dbConnect");
 
 
+dbConnect();
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log("Server started at port:",PORT);
