f50b24ef7a140836b9dec8e3d8e79abf1b3ce8b0
Harish Kunwar <kunwarharish11@gmail.com>
2024-03-01 11:06:21 +0545

    Implement authorization middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User=require("../models/userModel");
 const jwt=require("jsonwebtoken");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
 const CustomError=require("../utils/CustomError");
+const util=require("util");
 
 const signToken=(id)=>{
     return jwt.sign({id:id},process.env.JWT_SECRET,{expiresIn:'7d'});
@@ -31,4 +32,23 @@ exports.login=asyncErrorHandler(async (req,res,next)=>{
         status:"success",
         token
     });
+});
+
+//auth middleware
+exports.protectRoute=asyncErrorHandler(async (req,res,next)=>{
+    const authHeader=req.headers.authorization;
+    let token;
+    if(authHeader && authHeader.startsWith("Bearer")){
+        token=authHeader.split(" ")[1];
+    }
+    if(!token){
+        return next(new CustomError("You are not logged in",401));
+    }
+    const decoded=await util.promisify(jwt.verify)(token,process.env.JWT_SECRET);
+    const user=await User.findById(decoded.id);
+    if(!user){
+        return next(new CustomError("Invalid token",401));
+    }
+    req.user=user;
+    next();
 });
