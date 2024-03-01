132e606cee15ae255fdd4b3dedfe1035535ae2ae
Harish Kunwar <kunwarharish11@gmail.com>
2024-03-01 13:09:45 +0545

    Implement password change functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User=require("../models/userModel");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
+const CustomError=require("../utils/CustomError");
 
 //get user profile
 exports.getUserProfile=asyncErrorHandler(async (req,res,next)=>{
@@ -18,6 +19,9 @@ exports.getUserProfile=asyncErrorHandler(async (req,res,next)=>{
 //user updation
 exports.updateProfile=asyncErrorHandler(async (req,res,next)=>{
     const user=await User.findById(req.user._id);
+    if(!user){
+        return next(new CustomError("User not found",404));
+    }
     if(req.body.name){
         user.name=req.body.name;
     }
@@ -31,5 +35,26 @@ exports.updateProfile=asyncErrorHandler(async (req,res,next)=>{
     res.status(200).json({
         status:'success',
         message:"profile updated successfully"
+    });
+});
+
+//change password
+exports.changePassword=asyncErrorHandler(async (req,res,next)=>{
+    const {oldPassword,newPassword}=req.body;
+    const user=await User.findById(req.user._id).select("+password");
+    if(!user){
+        return next(new CustomError("User not found",404));
+    }
+    if(!oldPassword||!newPassword){
+        return next(new CustomError("please enter old and new password",404));
+    }
+    if(!(await user.comparePassword(oldPassword,user.password))){
+        return next(new CustomError("old password does not match",404));
+    }
+    user.password=newPassword;
+    await user.save();
+    res.status(200).json({
+        status:'success',
+        message:"password changed successfully"
     });
 });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const { protectRoute } = require("../controllers/authController");
-const { getUserProfile, updateProfile } = require("../controllers/userController");
+const { getUserProfile, updateProfile, changePassword } = require("../controllers/userController");
 
 const router=express.Router();
 
 router.get('/profile/:userId',protectRoute,getUserProfile);
 router.patch("/update-profile",protectRoute,updateProfile);
+router.patch("/change-password",protectRoute,changePassword);
 
 module.exports=router;
