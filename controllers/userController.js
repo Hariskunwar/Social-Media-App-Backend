c26aefd1eaac8a63bc315b97a09c8e7ca31820e8
Harish Kunwar <kunwarharish11@gmail.com>
2024-03-01 12:48:20 +0545

    Implement user update functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,22 @@ exports.getUserProfile=asyncErrorHandler(async (req,res,next)=>{
         }
     });
 });
+
+//user updation
+exports.updateProfile=asyncErrorHandler(async (req,res,next)=>{
+    const user=await User.findById(req.user._id);
+    if(req.body.name){
+        user.name=req.body.name;
+    }
+    if(req.body.email){
+        user.email=req.body.email;
+    }
+    if(req.body.bio){
+        user.bio=req.body.bio;
+    }
+    await user.save();
+    res.status(200).json({
+        status:'success',
+        message:"profile updated successfully"
+    });
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const { protectRoute } = require("../controllers/authController");
-const { getUserProfile } = require("../controllers/userController");
+const { getUserProfile, updateProfile } = require("../controllers/userController");
 
 const router=express.Router();
 
 router.get('/profile/:userId',protectRoute,getUserProfile);
+router.patch("/update-profile",protectRoute,updateProfile);
 
 module.exports=router;
